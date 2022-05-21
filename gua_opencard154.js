45fbf6163909e114a96de47f35742c96e202ab34
qionger0220 <dream1234.12@163.com>
2022-05-21 10:07:06 +0800

    Update gua_opencard154.js

--- gua_opencard154.js
+++ gua_opencard154.js
@@ -35,8 +35,8 @@ cron:30 1 18-31/3 5 *
 
 new Env('5.18~5.31 五月宠爱季 和你在一起')
 */
-let guaopencard_addSku = 'false'
-let guaopencard = 'false'
+let guaopencard_addSku = 'true'
+let guaopencard = 'true'
 let guaopenwait = '0'
 let guaopencard_draw = '0'
 
